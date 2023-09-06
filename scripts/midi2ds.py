80c9d6a10a0c04aab14b27506a9a668141c7a2d6
Tao Zhang <ztao8@hotmail.com>
2023-09-06 09:11:26 +0800

    Working commit.

--- scripts/midi2ds.py
+++ scripts/midi2ds.py
@@ -15,33 +15,217 @@
 # limitations under the License.
 
 import mido
+import json
 
 # default tempo per beat (in us):  120 beats per minute
 DEFAULT_TEMPO = 60000000 // 120
+DICTIONARY_FILE = "dictionaries/opencpop-extension.txt"
+PHONEME_FILE = "dictionaries/phoneme.txt"
+AP_TIME_IN_MS = 200
+
+# load dictionaries
+with open(DICTIONARY_FILE, "r", encoding="utf8") as f:
+    _dictionary = f.readlines()
+    _dictionary = [line.strip().split() for line in _dictionary]
+    _dictionary = {line[0]: line[1:] for line in _dictionary}
+
+with open(PHONEME_FILE, "r", encoding="utf8") as f:
+    _phoneme = f.readlines()
+    _phoneme = [line.strip().split() for line in _phoneme]
+    _phoneme = {line[0]: float(line[1]) for line in _phoneme}
 
 
-def midi2ds(midi_file: str, output_file: str) -> None:
+def _update_midi_info(midi_info: dict[int, dict[str]],
+                      current_time: int,
+                      tempo: int,
+                      syllable: str or None,
+                      note: int or None) -> None:
+    """ Update the MIDI information dictionary.
+
+        Args:
+            midi_info: the MIDI information dictionary to be updated.
+            tempo: current tempo in us per beat.
+            current_time: the current time in ticks.
+            syllable: the syllable of the current note.
+            note: the MIDI note of the current note, or 0 to turn off the current note.
+    """
+    if current_time not in midi_info:
+        midi_info[current_time] = {}
+    info = midi_info[current_time]
+    info["tempo"] = tempo
+    if syllable is not None:
+        info["syllable"] = syllable
+    if note is not None:
+        info["note"] = note
+
+
+def _update_duration(midi_info: list[(int, dict[str])]) -> None:
+    """ Update the duration of each note in the MIDI information list.
+
+        Args:
+            midi_info: the MIDI information list to be updated.
+    """
+    for i in range(len(midi_info) - 1):
+        duration = midi_info[i + 1][0] - midi_info[i][0]
+        midi_info[i][1]["duration"] = duration
+    midi_info[-1][1]["duration"] = 0
+
+
+def _update_phoneme(midi_info: list[(int, dict[str])]) -> None:
+    """ Update the phoneme of each note in the MIDI information list.
+
+        Args:
+            midi_info: the MIDI information list to be updated.
+    """
+    for mi in midi_info:
+        if "syllable" in mi[1] and mi[1]["syllable"] != "" and mi[1]["note"] != 0:
+            phoneme = _dictionary[mi[1]["syllable"]]
+            phoneme = [p.lower() for p in phoneme]
+
+            if len(phoneme) == 1:
+                mi[1]["phoneme"] = [(phoneme[0], mi[1]["duration"])]
+            else:
+                dur0 = _phoneme[phoneme[0]]
+                dur1 = _phoneme[phoneme[1]]
+                mi[1]["phoneme"] = [(phoneme[0], dur0 * mi[1]["duration"] / (dur0 + dur1)),
+                                    (phoneme[1], dur1 * mi[1]["duration"] / (dur0 + dur1))]
+
+
+def _align_midi_info(midi_info: list[(int, dict[str])],
+                     ticks_per_beat: int,
+                     lyrics: [str],
+                     syllables: [str]) -> None:
+    """Align the midi info list with lyrics and syllables.
+
+        Args:
+            midi_info: the MIDI information list to be aligned.
+            ticks_per_beat: number of ticks per beat in the MIDI file.
+            lyrics: the lyrics of the song.
+            syllables: the syllables of the song.
+
+        Returns:
+            The aligned MIDI information list.
+    """
+    syl = [s.split() for s in syllables]
+    lyc = [[c for c in l] for l in lyrics]
+
+    idx = 0
+
+    for i in range(len(syl)):
+        for j in range(len(syl[i])):
+            current_syllable_word = syl[i][j]
+            current_lyric_word = lyc[i][j]
+
+            while idx < len(midi_info) and (("syllable" not in midi_info[idx][1]) or
+                    midi_info[idx][1]["syllable"] != current_syllable_word):
+                if "syllable" in midi_info[idx][1]:
+                    if midi_info[idx][1]["syllable"] == "la":
+                        midi_info.remove(midi_info[idx])
+                        continue
+                    else:
+                        print(f"WARNING:  Lyric and syllable is not matched: {current_lyric_word} vs. {midi_info[idx][1]['syllable']}")
+                        break
+                midi_info[idx][1].pop("syllable", None)
+                midi_info[idx][1].pop("phoneme", None)
+
+                if midi_info[idx][1]["note"] == 0:
+                    midi_info[idx][1]["lyric"] = "<SP>"
+                    midi_info[idx][1]["syllable"] = "<SP>"
+                idx += 1
+
+            if j == 0 and idx > 0 and midi_info[idx - 1][1]["note"] == 0:
+                midi_info[idx - 1][1]["lyric"] = "<SP>"
+                ts = midi_info[idx - 1][0]
+                duration = midi_info[idx - 1][1]["duration"]
+                tempo = midi_info[idx - 1][1]["tempo"]
+                ap_duration = AP_TIME_IN_MS * ticks_per_beat * 1000 // tempo
+                if ap_duration < duration:
+                    midi_info[idx - 1][1]["duration"] = duration - ap_duration
+                    ts += ap_duration
+                    midi_info.insert(idx,
+                                     (ts, {"note": 0, "duration": ap_duration, "tempo": tempo,
+                                           "lyric": "<AP>", "syllable": "<AP>"}))
+
+            if idx >= len(midi_info):
+                raise Exception(f"Cannot find syllable {current_syllable_word} in MIDI information list.")
+
+            midi_info[idx][1]["lyric"] = current_lyric_word
+            idx += 1
+
+
+def _make_ds(midi_info: list[(int, dict[str])]) -> [dict[str]]:
+    """Make the DiffSinger file from midi information list.
+
+        Args:
+            midi_info: the MIDI information list.
+
+        Returns:
+            The DiffSinger file.
+    """
+    ds = []
+    return ds
+
+
+def midi2ds(midi_file: str,
+            lyrics: [str],
+            syllables: [str],
+            output_file: str) -> None:
     """ Convert an annotated MIDI file to a DiffSinger file.
 
         The MIDI file must be annotated with the following metadata:
             - lyrics: the lyrics of the song
     Args:
         midi_file: the annotated MIDI file.
+        lyrics: lyrics of the song, sentence by sentence.
+        syllables: syllables of the song, sentence by sentence.
         output_file: the output DiffSinger file.
     """
 
     tempo = DEFAULT_TEMPO
 
     midi = mido.MidiFile(midi_file)
+    ticks_per_beat = midi.ticks_per_beat
+    midi_info = {}
+
+    print(f"Ticks per beat: {ticks_per_beat}")
     for tid, track in enumerate(midi.tracks):
         print(f"Track {tid}: {track.name} ({len(track)} messages)")
+        current_time = 0
         for i, msg in enumerate(track):
+            # print(msg)
             if msg.type == "end_of_track":
                 break
-            if msg.type == "set_tempo":
+            elif msg.type == "set_tempo":
                 tempo = msg.tempo
                 print(f"  Updated tempo to {tempo}.")
-            print(msg)
+            elif msg.type == "note_on":
+                current_time += msg.time
+                if msg.velocity == 0:
+                    _update_midi_info(midi_info, current_time, tempo, None, 0)
+                else:
+                    _update_midi_info(midi_info, current_time, tempo, None, msg.note)
+            elif msg.type == "note_off":
+                current_time += msg.time
+                _update_midi_info(midi_info, current_time, tempo, None, 0)
+            elif msg.type == "lyrics":
+                current_time += msg.time
+                _update_midi_info(midi_info, current_time, tempo, msg.text, None)
+
         print(f"  End of track {tid}.")
 
+    midi_info = sorted(midi_info.items(), key=lambda x: x[0])
+    _update_duration(midi_info)
+    _update_phoneme(midi_info)
 
+    # Align the midi info with lyrics and syllables
+    _align_midi_info(midi_info, ticks_per_beat, lyrics, syllables)
+
+    print(midi_info)
+
+    # Make the DiffSinger file from midi info list
+    ds = _make_ds(midi_info)
+
+    # Write the DiffSinger file
+    ds_json_obj = json.dumps(ds, indent=2)
+    with open(output_file, "w", encoding="utf8") as f:
+        f.write(ds_json_obj)
--- scripts/midi2ds_12songs.py
+++ scripts/midi2ds_12songs.py
@@ -17,27 +17,109 @@
 import sys
 from pathlib import Path
 from midi2ds import midi2ds
+from pypinyin import pinyin, Style
+from zhon.hanzi import punctuation
 
 root_dir = Path(__file__).parent.parent.resolve()
 sys.path.insert(0, str(root_dir))
 
-NUM_MIDI_FILES = 12
+SOURCE_DIR = root_dir.joinpath("samples", "12-songs")
+SOURCE_LYRIC_FILE = SOURCE_DIR.joinpath("lyrics.txt")
+DEST_DIR = root_dir.joinpath("samples", "12-songs-ds")
+
+
+def _load_lyrics(lyric_file: Path) -> list[list[str]]:
+    """ Load the lyric file.
+
+        Args:
+            lyric_file: the lyric file to be loaded.
+
+        Returns:
+            A list of lyrics.
+    """
+    lyrics = []
+    with open(lyric_file, "r", encoding="utf8") as f:
+        lines = f.readlines()
+
+        # remove punctuations
+        for i in punctuation:
+            lines = [line.replace(i, "") for line in lines]
+
+        lines = [line.strip() for line in lines]
+
+        current_lyric = []
+        for line in lines:
+            if line == "" or line.startswith("#"):
+                if len(current_lyric) > 0:
+                    lyrics.append(current_lyric)
+                    current_lyric = []
+            elif line.startswith("[") or line.endswith("]"):
+                continue
+            else:
+                sentences = line.split()
+                for sentence in sentences:
+                    current_lyric.append(sentence.strip())
+        if len(current_lyric) > 0:
+            lyrics.append(current_lyric)
+
+    return lyrics
+
+
+def _pinyin_to_syllable(py: str) -> str:
+    """Get syllable from Pinyin by removing the tone number.
+
+        Args:
+            py: the Pinyin to be processed.
+
+        Returns:
+            The syllable.
+    """
+    while len(py) > 0 and py[-1].isdigit():
+        py = py[:-1]
+
+    return py
+
+
+def _lyrics_to_syllables(lyrics: list[list[str]]) -> list[list[str]]:
+    """ Convert the lyrics to syllables.
+
+        Args:
+            lyrics: the lyrics to be converted.
+
+        Returns:
+            A list of syllables.
+    """
+    syllables = []
+    for lyric in lyrics:
+        current_syllable = []
+        for sentence in lyric:
+            syllable = pinyin(sentence, style=Style.TONE3)
+            syllable = " ".join([_pinyin_to_syllable(s[0]) for s in syllable])
+            current_syllable.append(syllable)
+        syllables.append(current_syllable)
+    return syllables
+
 
 if __name__ == "__main__":
-    source_dir = root_dir.joinpath("samples", "12-songs")
-    dest_dir = root_dir.joinpath("samples", "12-songs-ds")
-
-    if not source_dir.exists():
-        print(f"Source directory {source_dir} does not exist.")
+    if not SOURCE_LYRIC_FILE.exists():
+        print(f"Source lyric file {SOURCE_LYRIC_FILE} does not exist.")
         sys.exit(1)
 
-    if not dest_dir.exists():
-        print(f"Destination directory {dest_dir} does not exist.")
+    if not SOURCE_DIR.exists():
+        print(f"Source directory {SOURCE_DIR} does not exist.")
         sys.exit(1)
 
-    midi_files = [source_dir / f"{i:d}/{i:d}.mid" for i in range(1, NUM_MIDI_FILES + 1)]
+    if not DEST_DIR.exists():
+        print(f"Destination directory {DEST_DIR} does not exist.")
+        sys.exit(1)
 
-    for file in midi_files:
+    lyrics = _load_lyrics(SOURCE_LYRIC_FILE)
+    syllables = _lyrics_to_syllables(lyrics)
+    midi_files = [SOURCE_DIR / f"{i:d}/{i:d}.mid" for i in range(1, len(lyrics) + 1)]
+
+    for i, file in enumerate(midi_files):
         print(f"Processing {file}...")
-        midi2ds(file, str(dest_dir.joinpath(Path(file).stem + ".ds")))
-        break
+        midi2ds(str(file),
+                lyrics[i],
+                syllables[i],
+                str(DEST_DIR.joinpath(Path(file).stem + ".ds")))
